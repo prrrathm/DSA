f335ec55af4e80ebccee7bb0c87186edaac41e4a
Pratham Sharma <prrrathm@gmail.com>
2021-09-22 01:38:00 +0530

    search linked list with recursion

--- LinkedList/searchll.cpp
+++ LinkedList/searchll.cpp
@@ -5,19 +5,41 @@ class node{
     public :
         int data;
         node* next;
-        node(int value){
+        node(int value) {
             data = value;
         }
 };
-string displayll(node* head){
-    if (head != NULL){
-        return "NULL\n";
+
+void displayll(node* head){
+    if (head == NULL){
+        cout << "NULL\n";
+        return;
     }
-    cout << head << "->";
-    return displayll(head->next);
+    cout << head->data << "->";
+    displayll(head->next);
+}
+
+void insertll(node* &head, int value){
+    if (head == NULL){
+        head = new node(value);
+        return;
+    }
+    insertll(head->next, value);
+}
+
+bool searchll(node* head, int key){
+    if(head == NULL){
+        return false;
+    }
+    if(head->data == key){
+        return true;
+    }
+    return searchll(head->next, key);
 }
 
 int main(){
-    node* head(0);
+    node* head = new node(0);
+    insertll(head, 1);
     displayll(head);
+    cout << searchll(head, 4);
 }
