c97e11a77d039316e267858b0304c0276cad9136
Pratham Sharma <prrrathm@gmail.com>
2021-09-22 19:58:55 +0530

    template insert next

--- LinkedList/lltemplate.cpp
+++ LinkedList/lltemplate.cpp
@@ -3,17 +3,15 @@ using namespace std;
 
 template <class T> class Node{
     public :
-        T data;
+        T data = 1;
         Node<T> *next;
-        // Node(T value){
-        //     data = value;
-        //     next = NULL;
-        // }
+        Node(T value){
+            data = value;
+            next = NULL;
+        }
         void insertNext(T value){
-            // Node<T> *tem = this;
             if (this->next == NULL){
-                this->next = new Node;
-                this->next->data = value;
+                this = new Node<int>(value);
                 return;
             }
             this->next->insertNext(value);
@@ -23,7 +21,9 @@ template <class T> class Node{
 template <class T> void insertNex(Node<T> head, int value);
 
 int main(){
-    Node<int> *head(0);
+    Node<int> *head = new Node<int>(0);
     head->insertNext(1);
+    cout<< head->data;
+    cout<< head->next;
     return 0;
 }
