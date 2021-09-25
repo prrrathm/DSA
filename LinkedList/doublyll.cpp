0934256a3de5b65b1b50af7423c7c0b1dde26f6f
Pratham Sharma <prrrathm@gmail.com>
2021-09-25 21:49:01 +0530

    insert next doubly ll

--- LinkedList/doublyll.cpp
+++ LinkedList/doublyll.cpp
@@ -4,14 +4,35 @@ using namespace std;
 template <class T> class Node{
     public :
         int data;
-        Node<T> *head;
-        Node<T> *tail;
-        Node(T value){
+        Node<T> *next;
+        Node<T> *prev;
+        Node(T value) {
             data = value;
+        }
+        
+        void display(){
+            if(this == NULL){
+                cout << "NULL\n";
+                return;
+            }
+            cout << this->data << "->";
+            this->next->display();
+        }
+        void insertNext(T value){
+            if(this == NULL){
+                this = new Node<T>(value);
+            }
+            if(this->next == NULL){
+                this->next = new Node<T>(value);
+                return;
+            }
+            this->next->insertNext(value);
         }
 };
 
 int main(){
-
+    Node<int> *head = new Node<int>(0);
+    head->insertNext(1);
+    head->display();
     return 0;
 }
