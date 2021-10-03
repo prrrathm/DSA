6cc449c41755c887e3b2e45df23b4859387db0d2
Pratham Sharma <prrrathm@gmail.com>
2021-10-04 01:50:59 +0530

    insert corrected

--- LinkedList/lltemplate.cpp
+++ LinkedList/lltemplate.cpp
@@ -30,23 +30,46 @@ template<typename T>class LinkedList{
         cout << "NULL\n";
       }
       void insertNext(T value){
+          Node<T> *temp = head;
           if(head == NULL){
             head = new Node<T>(value);
             return;
           }
+          while(temp->next != NULL){
+            temp = temp->next;
+          }
+          temp->next = new Node<T>(value);
       }
       void inserBefore(T value){
         Node<T>* temp = head;
         head = new Node<T>(value);
         head->next = temp;
       }
+      int size(){
+        Node<T>* temp = head;
+        int size = 0;
+        while(temp != NULL){
+          size++;
+          cout << size;
+          temp = temp->next;
+        }
+        return size;
+      }
+      void insert(int index, T value){
+        if (value > head->size()){
+          cout << "Index bigger than size of LinkedList" << '\n';
+          return;
+        }
 
+      }
 };
 
 int main(){
   LinkedList<int> A;
   A.insertNext(1);
+  A.insertNext(3);
   A.inserBefore(2);
   A.display();
+  cout << A.size() << endl;
   return 0;
 }
