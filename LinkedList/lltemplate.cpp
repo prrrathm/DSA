1e178676e8b128648f61454c9d1b89d47b0244b7
Pratham Sharma <prrrathm@gmail.com>
2021-10-04 01:33:13 +0530

    insert before template

--- LinkedList/lltemplate.cpp
+++ LinkedList/lltemplate.cpp
@@ -2,45 +2,51 @@
 using namespace std;
 
 template<typename T>class Node{
+  private:
+      T data;
+      Node<T>* next;
+      template<typename U>friend class LinkedList;
   public:
-        T data;
-        Node<T>* next;
-        template<typename U>friend class LinkedList;
-    public:
-        Node(){
-            this->next = NULL;
-        }
-        Node(T value){
-            this->data = value;
-            this->next = NULL;
-        }
+      Node(){
+          this->next = NULL;
+      }
+      Node(T value){
+          this->data = value;
+          this->next = NULL;
+      }
 };
 template<typename T>class LinkedList{
     private:
         Node<T>* head;
     public:
-        LinkedList(){
-            this->head = NULL;
+      LinkedList(){
+        this->head = NULL;
+      }
+      void display(){
+        while(head != NULL){
+          cout << head->data << "->";
+          head = head->next;
         }
-        void insertNext(T value){
-            if(head == NULL){
-                head = new Node<T>(value);
-                return;
-            }
-        }
-        void display(){
-          while(head != NULL){
-            cout << head->data << "->";
-            head = head->next;
+        cout << "NULL\n";
+      }
+      void insertNext(T value){
+          if(head == NULL){
+            head = new Node<T>(value);
+            return;
           }
-            cout << "NULL\n";
-        }
+      }
+      void inserBefore(T value){
+        Node<T>* temp = head;
+        head = new Node<T>(value);
+        head->next = temp;
+      }
+
 };
 
 int main(){
   LinkedList<int> A;
   A.insertNext(1);
-  // cout << A.data;
+  A.inserBefore(2);
   A.display();
   return 0;
 }
