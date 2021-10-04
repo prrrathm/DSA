d79b56c5aff9c561310386baf18b7d3ab581447c
Pratham Sharma <prrrathm@gmail.com>
2021-10-04 07:38:17 +0530

    doubly ll created

--- LinkedList/doublyll.cpp
+++ LinkedList/doublyll.cpp
@@ -1,43 +1,56 @@
 #include <iostream>
 using namespace std;
 
-template <typename T> class Node{
-    public :
-        int data;
-        Node<T> *next;
-        Node<T> *prev;
-        static bool headEmpty;
-        Node(){
-            headEmpty = false;
-        }
-        Node(T value){
-            data = value;
-            headEmpty = true;
-        }
-        void display(){
-            if(this == NULL){
-                cout << "NULL\n";
-                return;
-            }
-            cout << this->data << "->";
-            this->next->display();
-        }
-        void insertNext(T value){
-            if(this->next == NULL){
-                if(headEmpty == true){
-                    this->data = value;
-                    return;
-                }
-                this->next = new Node<T>(value);
-                return;
-            }
-            this->next->insertNext(value);
-        }
+template <typename A> class Node{
+  private :
+    A data;
+    Node<A>* next;
+    Node<A>* prev;
+    template <typename B> friend class Doublell;
+  public:
+    Node(){
+      this->prev = NULL;
+      this->next = NULL;
+    }
+    Node(A value){
+      this->data = value;
+      this->prev = NULL;
+      this->next = NULL;
+    }
+};
+
+template<typename A> class Doublell{
+  Node<A>* head;
+  public:
+    Doublell(){
+      head = NULL;
+    }
+    void display(){
+      Node<A>* temp = head;
+      while(temp != NULL){
+        cout << temp->data <<"->";
+        temp= temp->next;
+      }
+      cout << "NULL" << '\n';
+    }
+    void append(int value){
+      if (head == NULL){
+        head = new Node<A>(value);
+        return;
+      }
+      Node<A>* temp = head;
+      while(temp->next != NULL){
+        temp = temp->next;
+      }
+      temp->next = new Node<A>(value);
+      temp->next->prev = temp->next;
+    }
 };
 
 int main(){
-    Node<int> *head = new Node<int>();
-    head->insertNext(1);
-    head->display();
-    return 0;
+  Doublell<int> dll;
+  dll.display();
+  dll.append(2);
+  dll.display();
+  return 0;
 }
