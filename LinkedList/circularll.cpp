131686e17148ad550cffc0c8b1fc31ed5e117541
Pratham Sharma <prrrathm@gmail.com>
2021-10-12 18:59:45 +0530

    insertnext added

--- LinkedList/circularll.cpp
+++ LinkedList/circularll.cpp
@@ -16,7 +16,20 @@ template<typename A> class Node{
 
 template<typename A> class circularll{
     Node<A>* head;
-    
+    circularll(){
+        head = new Node<A>();
+    }
+    void insertNext(A value){
+        if(head == NULL){
+            head = new Node<A>(value);
+        }
+        Node<A>* temp = head;
+        while(temp->next != head){
+            temp = temp->next;
+        }
+        temp->next = new Node<A>(value);
+        temp->next->next = head;
+    }
 };
 
 int main(){
